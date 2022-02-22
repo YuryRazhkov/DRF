63248abb98706b6b996c0241c659dd5a4767a9bd
YURY <yrozhkov1983@gmail.com>
2022-02-22 12:10:11 +0300

    create userapp

---
+++ todo/userapp/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+   email = models.EmailField('email address', blank=True, unique=True)
+
+
+
---
+++ todo/userapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework.viewsets import ModelViewSet
+from .serializer import UserModelSerializer
+from .models import User
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserModelSerializer
+    queryset = User.objects.all()
