790276e424bbd4445d92e4860b57712d9e1e9762
YURY <yrozhkov1983@gmail.com>
2022-02-23 06:58:47 +0300

    lesson-2

---
+++ frontend/src/components/UseList.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+//
+//
+// const AuthorItem = ({us}) => {
+//    return (
+//        <tr>
+//            <td>
+//                {author.first_name}
+//            </td>
+//            <td>
+//                {author.last_name}
+//            </td>
+//            <td>
+//                {author.birthday_year}
+//            </td>
+//        </tr>
+//    )
+// }
